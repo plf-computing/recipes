23b1b97b2bc8171252db51360e951767bd5910ae
plf-computing <plfcomputing@gmail.com>
2023-04-05 12:05:15 +0300

    recipe names and descriptions

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -6,7 +6,7 @@ import { Recipe} from '../recipes.model';
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent {
   recipes: Recipe[] =[
     new Recipe('pizza','cook like this','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
   ];
@@ -15,7 +15,7 @@ export class RecipesListComponent implements OnInit {
 
   constructor(){}
 
-  ngOnInIt(){
+  ngOnInIt():void{
 
   }
 
